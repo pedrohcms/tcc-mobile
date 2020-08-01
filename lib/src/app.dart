47bbbd53ccf28c54567111209e6830ca10887785
pedrohcms <pedrohcms2018@gmail.com>
2020-08-01 10:41:45 -0300

    Refactoring project

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World'),
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
+}
