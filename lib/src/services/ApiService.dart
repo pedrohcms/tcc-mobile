051c05dbd80da2f7ea56cce6887fe0f47f49150c
pedrohcms <pedrohcms2018@gmail.com>
2020-08-09 14:34:34 -0300

    Finished new ApiService class

--- lib/src/services/ApiService.dart
+++ lib/src/services/ApiService.dart
@@ -1,15 +1,32 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:http/http.dart';
 
 class ApiService {
   final Client _client = new Client();
   final String _baseUrl = 'http://10.0.0.167:3333';
+  final int _timeOut = 5;
 
+  /// Responsible for making the request to the API
+  ///
+  /// Can throw the following exceptions:
+  ///
+  /// [SocketException] in case the device does not have internet connection;
+  ///
+  /// [TimeoutException] in case the API takes to long to respond;
+  ///
+  /// [FormatException] in case the method param is diferent then GET, POST, UPDATE or DELETE;
+  ///
+  /// [HttpException] in case the server responds with 500/Internal Server Error status;
   Future<Response> makeRequest(
       {String method,
       String uri,
       String body,
       Map<String, dynamic> headers,
       Map<String, dynamic> queries}) async {
+    // Checking for internet connection
+    await InternetAddress.lookup('google.com');
+
     // Setting up the URL
     String url = '$_baseUrl/$uri' + makeQuery(queries);
 
@@ -20,26 +37,57 @@ class ApiService {
 
     headers['Content-Type'] = 'application/json; charset=utf-8';
 
+    Response response;
+
     // Sending the request
     switch (method) {
       case 'GET':
-        return await _client.get(url, headers: headers);
+        response = await _client.get(url, headers: headers).timeout(
+          Duration(seconds: _timeOut),
+          onTimeout: () {
+            throw TimeoutException('Connection timed out');
+          },
+        );
         break;
 
       case 'POST':
-        return await _client.post(url, body: body, headers: headers);
+        response =
+            await _client.post(url, body: body, headers: headers).timeout(
+          Duration(seconds: _timeOut),
+          onTimeout: () {
+            throw TimeoutException('Connection timed out');
+          },
+        );
         break;
 
       case 'UPDATE':
-        return await _client.put(url, body: body, headers: headers);
+        response = await _client.put(url, body: body, headers: headers).timeout(
+          Duration(seconds: _timeOut),
+          onTimeout: () {
+            throw TimeoutException('Connection timed out');
+          },
+        );
         break;
 
       case 'DELETE':
-        return await _client.delete(url, headers: headers);
+        response = await _client.delete(url, headers: headers).timeout(
+          Duration(seconds: _timeOut),
+          onTimeout: () {
+            closeClient();
+            throw TimeoutException('Connection timed out');
+          },
+        );
         break;
       default:
         throw new FormatException('Method not found');
+        break;
     }
+
+    if (response.statusCode == 500) {
+      throw new HttpException('Internal Server Error');
+    }
+
+    return response;
   }
 
   String makeQuery(Map<String, dynamic> queries) {
