0190b0877b9f27cca1ac41125fe2e02efccfe78a
pedrohcms <pedrohcms2018@gmail.com>
2020-10-10 23:14:04 -0300

    Criando componente que mostra quantidade de agua e adicionando formataçao para numeros

--- lib/src/pages/HomePage/HomePage.dart
+++ lib/src/pages/HomePage/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/src/components/WaterAmountComponent.dart';
+import 'package:mobile/src/services/TokenService.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,9 +18,11 @@ class _HomePageState extends State<HomePage> {
             expandedHeight: 175.0,
             backgroundColor: Colors.white,
             flexibleSpace: FlexibleSpaceBar(
-              //title: Text("Bem vindo a sua propriedade"),
-              background:
-                  Image.asset('images/home.png', height: 210, width: 210),
+              background: Image.asset(
+                'images/home.png',
+                height: 210,
+                width: 210,
+              ),
             ),
           ),
           SliverAppBar(
@@ -27,81 +31,29 @@ class _HomePageState extends State<HomePage> {
             expandedHeight: 50.0,
             backgroundColor: Colors.blue,
             flexibleSpace: FlexibleSpaceBar(
-              title: Text("Bem vindo a sua Propriedade"),
+              title: Text(
+                "Bem vindo a sua Propriedade",
+              ),
             ),
           ),
           SliverFixedExtentList(
             itemExtent: 150.0,
             delegate: SliverChildListDelegate(
               [
-                //PRIMEIRO CONTAINER
-                ClipRRect(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.blue,
-                    ),
-                  ),
+                WaterAmountComponent(
+                  text: "QUANTIDADE DE ÁGUA USADA HOJE",
+                  amount: 90000.25,
+                  color: Colors.blue[200],
                 ),
-
-                //SEGUNDO CONTAINER
-                ClipRRect(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.blue[200],
-                    ),
-                  ),
+                WaterAmountComponent(
+                  text: "QUANTIDADE DE ÁGUA USADA NAS ÚLTIMAS 12 HORAS",
+                  amount: 100000,
+                  color: Colors.blue,
                 ),
-
-                //TERCEIRO CONTAINER
-                ClipRRect(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.blue[500],
-                    ),
-                  ),
-                ),
-
-                //QUARTO CONTAINER
-                ClipRRect(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.blue[700],
-                    ),
-                  ),
-                ),
-
-                // QUINTO CONTAINER
-                ClipRRect(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.blue[900],
-                    ),
-                  ),
+                WaterAmountComponent(
+                  text: "QUANTIDADE DE ÁGUA USADA NAS UĹTIMAS 24 HORAS",
+                  amount: 250000,
+                  color: Colors.blue[700],
                 ),
                 Container(color: Colors.white),
               ],
@@ -179,6 +131,8 @@ class _HomePageState extends State<HomePage> {
                     ],
                   ),
                   onPressed: () {
+                    TokenService.deleteToken();
+
                     Navigator.popAndPushNamed(context, '/');
                   },
                 ),
