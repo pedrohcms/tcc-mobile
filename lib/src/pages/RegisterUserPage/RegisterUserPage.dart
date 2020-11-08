c49beeb34f1fea8224416aaab70d5f926eb35d50
Gustavo A M Costa <51254837+gustavoamc@users.noreply.github.com>
2020-11-08 12:41:26 -0300

    userBloc finished

---
+++ lib/src/pages/RegisterUserPage/RegisterUserPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterUserPage extends StatefulWidget {
+  @override
+  _RegisterUserPageState createState() => _RegisterUserPageState();
+}
+
+class _RegisterUserPageState extends State<RegisterUserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
